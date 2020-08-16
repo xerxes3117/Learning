f3966e7c3fb086d977279c110e7a01b9efe61a73
xerxes3117 <vsnsit@gmail.com>
2020-08-16 23:21:50 +0530

    score js updated

--- ml/stephen-grder-course/MLKits/plinko/score.js
+++ ml/stephen-grder-course/MLKits/plinko/score.js
@@ -1,12 +1,27 @@
 const outputs = [];
+const predictionPoint = 300;
+const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
-  console.log(outputs);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+  const bucket = _.chain(outputs)
+                  .map(row => [distance(row[0]), row[3]])
+                  .sortBy(row => row[0])
+                  .slice(0, k)
+                  .countBy(row => row[1])
+                  .toPairs()
+                  .sortBy(row => row[1])
+                  .last()
+                  .first()
+                  .value();
+
+  console.log("You're point will fall into: ", bucket);
+}
+
+function distance(point){
+  return Math.abs(predictionPoint - point);
 }
 
