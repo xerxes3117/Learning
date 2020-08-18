3a6e8392972b7667aedd7d52c30eb8fcfc46b9e6
xerxes3117 <vsnsit@gmail.com>
2020-08-18 15:45:58 +0530

    golang mux api

--- go/goworkspace/src/github.com/basic-golang-mux-api/http-api.go
+++ go/goworkspace/src/github.com/basic-golang-mux-api/http-api.go
@@ -7,36 +7,39 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var data []string = []string{}
+//Item is a struct that groups all necessary fields into a sin unit
+type Item struct {
+	Data      string `json:"data"`
+	OtherData int    `json:"otherData"`
+}
+
+var data []Item = []Item{}
 
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/test", test)
-
 	//{item} is a dynamic variable in url that can be accessed by router function
-	router.HandleFunc("/add/{item}", addItem)
+	router.HandleFunc("/add", addItem).Methods("POST")
 
 	http.ListenAndServe(":5000", router)
 }
 
 /**
- * @todo what does json.NewEncoder do?
- */
-func test(w http.ResponseWriter, r *http.Request) {
+ * What does json.NewEncoder and Decoder do?
+	- json.NewEncoder.Encode : golang data structure -> json format
+	- json.NewDecoder.Decode : json format -> golang data structure
+ * @todo Is converting json <-> utf-8 also part of json.NewEncoder and json.NewDecoder?
+ * @todo marshal/unmarshal vs encode/decode
+*/
+func addItem(w http.ResponseWriter, req *http.Request) {
+	//Get Item value from request body
+	var newItem Item
+	json.NewDecoder(req.Body).Decode(&newItem)
+
+	data = append(data, newItem)
+
 	//Set the content-type to for response
 	w.Header().Set("Content-Type", "application/json")
 
-	// Create a json encoding from anonymous Struct provided
-	json.NewEncoder(w).Encode(struct {
-		ID string
-	}{"555"})
-}
-
-func addItem(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	routeVar := mux.Vars(req)["item"]
-	data = append(data, routeVar)
 	json.NewEncoder(w).Encode(data)
 }
