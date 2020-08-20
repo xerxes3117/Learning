b643eae01f0c05117f2601a0abcaca8a421f3cdd
xerxes3117 <vsnsit@gmail.com>
2020-08-21 00:52:01 +0530

    golang practice

--- go/goworkspace/src/github.com/basic-golang-mux-api/http-api.go
+++ go/goworkspace/src/github.com/basic-golang-mux-api/http-api.go
@@ -1,12 +1,30 @@
 /**
- * * What does json.NewEncoder and Decoder do?
- * * - json.NewEncoder.Encode : golang data structure -> json format
- * * - json.NewDecoder.Decode : json format -> golang data structure
- * ? Is converting json <-> utf-8 also part of json.NewEncoder and json.NewDecoder?
- * ? marshal/unmarshal vs encode/decode
+ * ? 1. What does json.NewEncoder and Decoder do?
+ * *  - json.NewEncoder.Encode : golang data structure -> json format -> write to http stream
+ * *  - json.NewDecoder.Decode : read from http stream -> json format -> golang data structure
+
+ * ? 2. Is converting json <-> utf-8 also part of json.NewEncoder and json.NewDecoder?
+ * *  - Yes, strings in golang are already UTF-8 encoded
+
+ * ? 3. marshal/unmarshal vs encode/decode
+ * *  - Marshal and Unmarshal mean that we want to get the JSON representation of any type and vice-versa.
+ * *    While Encode means that we want to do the Marshaling process and then write(encode) the result to any stream object.
+ * *	And Decode means that we want to get(decode) a json object from any stream and then do the Unmarshaling process.
+
+ * ? How do we check for invalid format data coming in body (eg. incoming array of posts rather than single post in PUT request)
+ * ? how are data structures stored in memory
+ * ? how a program is executed
+ * ? what exactly is happening in terms of binary values when we serialize/deserialize data to json
+ * ? same as above for when we encode utf-8 data to other formats
+ * ? is binary representation of data changing during encoding?
+ * ? What is meant by json format. What is json format of various data structures
  * ? how is append working?
  * ? what are http.ResponseWriter and *http.Request in route handler functions?
  * ? what is w.Write and w.WriteHeader doing?
+ * ? What is slice of bytes?
+ * ? JSON vs string vs byte slice in golang
+ * ? Why Golang has json in slice of bytes format
+ * ? How is this working: append(posts[:id], posts[id+1:]...)
  */
 
 package main
@@ -38,12 +56,18 @@ var posts []Post = []Post{}
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/posts", addItem).Methods("POST")
+	//.Methods is the list of allowed methods on that route. It accepts a variadic parameter
+	router.HandleFunc("/posts", addPost).Methods("POST")
 
 	router.HandleFunc("/posts", getAllPosts).Methods("GET")
 
-	//{id} is a route variable in url that can be accessed by router function
 	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
+
+	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+
+	router.HandleFunc("/posts/{id}", patchPost).Methods("PATCH")
+
+	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
 
 	http.ListenAndServe(":5000", router)
 }
@@ -62,6 +86,7 @@ func getPost(w http.ResponseWriter, r *http.Request) {
 	if id >= len(posts) {
 		w.WriteHeader(404)
 		w.Write([]byte("No post found with id"))
+		return
 	}
 
 	post := posts[id]
@@ -76,7 +101,7 @@ func getAllPosts(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(posts)
 }
 
-func addItem(w http.ResponseWriter, req *http.Request) {
+func addPost(w http.ResponseWriter, req *http.Request) {
 	//Get Item value from request body
 	var newPost Post
 	json.NewDecoder(req.Body).Decode(&newPost)
@@ -87,4 +112,75 @@ func addItem(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	json.NewEncoder(w).Encode(posts)
+}
+
+func updatePost(w http.ResponseWriter, r *http.Request) {
+	// get the post ID from the route params
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID cannot be converted to an integer"))
+	}
+
+	// error checking
+	if id >= len(posts) {
+		w.WriteHeader(404)
+		w.Write([]byte("No post found with id"))
+		return
+	}
+
+	// get the value from the JSON body
+	var updatedPost Post
+	json.NewDecoder(r.Body).Decode(&updatedPost)
+
+	posts[id] = updatedPost
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedPost)
+}
+
+func patchPost(w http.ResponseWriter, r *http.Request) {
+	// get the post ID from the route params
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID cannot be converted to an integer"))
+	}
+
+	// error checking
+	if id >= len(posts) {
+		w.WriteHeader(404)
+		w.Write([]byte("No post found with id"))
+		return
+	}
+
+	//get the current value
+	post := &posts[id]
+	json.NewDecoder(r.Body).Decode(post)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(post)
+}
+
+func deletePost(w http.ResponseWriter, r *http.Request) {
+	// get the post ID from the route params
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID cannot be converted to an integer"))
+	}
+
+	// error checking
+	if id >= len(posts) {
+		w.WriteHeader(404)
+		w.Write([]byte("No post found with id"))
+		return
+	}
+
+	posts = append(posts[:id], posts[id+1:]...)
+
+	w.WriteHeader(http.StatusOK) // 200
 }
---
+++ go/goworkspace/src/github.com/practice/understanding-strings-and-encodings.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// Person struct
+type Person struct {
+	F string
+	L string
+}
+
+func main() {
+	//1.String is internally stored as a byte slice
+
+	//Byte slices are a list of bytes that represent UTF-8 encodings of Unicode code points of each character in the string
+	str := "Hello 世界 there"
+	bs := []byte(str)
+
+	fmt.Println(bs)  // [72 101 108 108 111 32 228 184 150 231 149 140 32 116 104 101 114 101]
+	fmt.Println(str) // Hello 世界 there
+
+	//2. Showing string representation of byte slice
+
+	fmt.Printf("%s\n", bs)
+	fmt.Println(string(bs))
+
+	//3.Runes vs bytes
+
+	//Rune corresposnds to a character's Unicode code point
+	//A character's Unicode code point can be represented by 1 to 4 bytes (depending on the character)
+	rs := []rune(str)
+
+	fmt.Println(rs) // [72 101 108 108 111 32 19990 30028 32 116 104 101 114 101]
+
+	//4.Binary representation of strings
+
+	//Note that 世 & 界 are stored in 3 bytes each which confirms strings in golang are UTF-8 encoded slice of bytes (rather than direct binary representation of 19990 & 30028 as in runes)
+	st := fmt.Sprintf("%08b", []byte(str))
+	fmt.Println(st) // [01001000 01100101 01101100 01101100 01101111 00100000 11100100 10111000 10010110 11100111 10010101 10001100 00100000 01110100 01101000 01100101 01110010 01100101]
+
+	//This is how strings would be stored if they were rune slices
+	st1 := fmt.Sprintf("%08b", []rune(str))
+	fmt.Println(st1) // [01001000 01100101 01101100 01101100 01101111 00100000 100111000010110 111010101001100 00100000 01110100 01101000 01100101 01110010 01100101]
+
+	//5. Range loops over elements in runes and not bytes
+
+	// however counter i is incremented by number of bytes travsersed rather than runes
+	for i, b := range str {
+		fmt.Printf("i: %d. b: %q\n", i, b)
+	}
+
+	//6. Marshalling and unmarshalling to/from JSON in golang
+	p1 := Person{"a", "b"}
+	bs1, _ := json.Marshal(p1)
+	fmt.Println(bs1)
+	fmt.Println(string(bs1))
+
+	var p2 Person
+	bs1 = []byte(`{"First":"alice","Last":"bob"}`)
+	json.Unmarshal(bs1, &p2)
+	fmt.Println(p2)
+
+}
