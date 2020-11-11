068de9d7a6179fe70df2d604f536b6d17f871efa
xerxes3117 <vsnsit@gmail.com>
2020-11-12 00:03:23 +0530

    changes

--- nodejs/net-ninja-tutorial/app.js
+++ nodejs/net-ninja-tutorial/app.js
@@ -1,9 +1,16 @@
 var http = require('http');
+var fs = require('fs');
+
 
 var server = http.createServer(function(req, res) {
     console.log('Request was made: ' + req.url)
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!!');
+
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    var myObj = {
+        name: 'Vaibhav',
+        age: 29
+    };
+    res.end(JSON.stringify(myObj));
 });
 
 server.listen(3000, '127.0.0.1');
