c771f65b5c97d36906cb83ccd52d6a28dc6b8cac
xerxes3117 <vsnsit@gmail.com>
2020-11-11 12:08:31 +0530

    node tutorial

--- nodejs/net-ninja-tutorial/app.js
+++ nodejs/net-ninja-tutorial/app.js
@@ -1,1 +1,10 @@
-var event = require('events');
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    console.log('Request was made: ' + req.url)
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!!');
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('Now listerning on port 3000...')
