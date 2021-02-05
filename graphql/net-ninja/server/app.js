552561a433f7f547366d0d67717faace48f71443
xerxes3117 <vsnsit@gmail.com>
2021-02-06 00:28:00 +0530

    netninga graphql upto #20

--- graphql/net-ninja/server/app.js
+++ graphql/net-ninja/server/app.js
@@ -1,8 +1,17 @@
- const express = require('express');
+const express = require('express');
 const {graphqlHTTP} = require('express-graphql')
 const mySchema  = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express();
+
+//Database connection
+//testgql is our database
+//book and author are collections in this database
+mongoose.connect('mongodb+srv://admin:combination@cluster0.e8xrn.mongodb.net/testgql?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+  console.log('connected to MongoDB')
+})
 
 //Adding graphql as a middleware
 //Basically this is telling Node that any request to route '/graphql' can be handled through this graphql schema code
--- graphql/net-ninja/server/schema/schema.js
+++ graphql/net-ninja/server/schema/schema.js
@@ -1,12 +1,16 @@
 const graphql = require('graphql');
+const {
+  GraphQLObjectType, 
+  GraphQLString, 
+  GraphQLSchema, 
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull
+} = graphql;
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
-
-const books = [
-  {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
-  {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
-  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'}
-]
+const Book = require('../models/book')
+const Author = require('../models/author')
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -14,9 +18,35 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLString},
     name: { type: GraphQLString},
     genre: { type: GraphQLString},
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        //'parent' here refers to parent data object in which this resolve is called (i.e. book object here)
+        return Author.findById(parent.authorId)
+      }
+    }
   })
 })
 
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLString},
+    name: { type: GraphQLString},
+    age: { type: GraphQLInt},
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return Book.find(parent.id)
+      }
+    }
+  })
+})
+
+/**
+ * List of RootQueries
+ * RootQueries are used to access data using graphql query
+ */
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   //fields consists of all the rootqueries that can access graphql data
@@ -25,15 +55,82 @@ const RootQuery = new GraphQLObjectType({
     book: {
       type: BookType,
       //args can used in graphQL query to access book data by a particular argument (eg. if we want to get book with id = '123')
-      args: {id: { type: GraphQLString}}, 
+      args: {id: {type: GraphQLID}}, 
       resolve(parent, args){
         //code to get data from DB
-        return books.find(book => book.id == args.id)
+        return Book.findById(args.id)
+      }
+    },
+    //author is a root query to access a AuthorType object
+    author: {
+      type: AuthorType,
+      args: {id: {type: GraphQLID}},
+      resolve(parent, args){
+        return Author.findById(args.id)
+      }
+    },
+    //query to fetch all books
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args){
+        //return books
+        return Book.find({})
+      }
+    },
+    //query to fetch all authors
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args){
+        //return authors
+        return Author.find({})
       }
     }
   }
 })
 
+/**
+ * List of mutations
+ * Mutations are used to update data through a graphql query
+ */
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString)},
+        age: { type: new GraphQLNonNull(GraphQLInt)},
+      },
+      resolve(parent, args) {
+        //Create a author using mongoose model 'Author'
+        let author = new Author({
+          name: args.name,
+          age: args.age
+        });
+        return author.save();
+      }
+    },
+    addBook: {
+      type: BookType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString)},
+        genre: { type: new GraphQLNonNull(GraphQLString)},
+        authorId: {type: new GraphQLNonNull(GraphQLString)}
+      },
+      resolve(parent, args) {
+        //Create a book using mongoose model 'Book'
+        let book = new Book({
+          name: args.name,
+          genre: args.genre,
+          authorId: args.authorId
+        });
+        return book.save();
+      }
+    }
+  }
+}) 
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: Mutation
 })
