2a5962140b20bc8ccc8a39f9325f5aa9d56135ed
xerxes3117 <vsnsit@gmail.com>
2021-03-13 15:07:34 +0530

    yelp pern BE done

--- full_stack/yelp-pern-fcc/Notes.md
+++ full_stack/yelp-pern-fcc/Notes.md
@@ -1,1 +1,7 @@
+<!-- Notes -->
 1. Use parametrized sql queries to prevent SQL injection
+
+<!-- Stuff that is missing from this tutorial as compared to a real project -->
+1. No defining of schema for the database in code (that was done manually in psql which is not ideal)
+2. No use of data or db migrations
+3. Did not define a separate folder to store all resource controllers (although i did it)
--- full_stack/yelp-pern-fcc/server/controllers/restaurantController.js
+++ full_stack/yelp-pern-fcc/server/controllers/restaurantController.js
@@ -4,7 +4,7 @@ module.exports = function(app, db) {
   app.get('/api/v1/restaurants', async (req, res) => {
     try {
       const results = await db.query('select * from restaurants');
-      res.status(200).send({
+      res.status(200).json({
         status: 'success',
         results: results.rows.length,
         data: {
@@ -20,7 +20,7 @@ module.exports = function(app, db) {
   app.get('/api/v1/restaurants/:id', async (req, res) => {
     try {
       const result = await db.query('select * from restaurants where id = $1', [req.params.id])
-      res.status(200).send({
+      res.status(200).json({
         status: 'success',
         data: {
           restaurant : result.rows.length > 0 ? result.rows[0] : 'No restaurant found with this id'
@@ -37,7 +37,7 @@ module.exports = function(app, db) {
       const {name, location, price_range} = req.body;
       const results = await db.query('insert into restaurants (name, location, price_range) values($1, $2, $3) returning *', [name, location, price_range])
 
-      res.status(201).send({
+      res.status(201).json({
         status: 'success',
         data: {
           restaurant: results.rows[0]
@@ -49,24 +49,30 @@ module.exports = function(app, db) {
   })
 
   //Update restaurant
-  app.put('/api/v1/restaurants/:id', (req, res) => {
-    console.log(req.params.id);
-    console.log(req.body);
-    res.status(200).send({
-      status: 'success',
-      data: {
-        id: req.params.id
-      }
-    })
+  app.put('/api/v1/restaurants/:id', async (req, res) => {
+    try {
+      const {name, location, price_range} = req.body;
+      const result = await db.query('update restaurants set name = $1, location = $2, price_range = $3 where id = $4 returning *', [name, location, price_range, req.params.id]);
+      res.status(200).json({
+        status: 'success',
+        data: {
+          restaurant: result.rows.length > 0 ? result.rows[0] : 'No restaurant found with this id'
+        }
+      })
+    } catch (err) {
+      console.log(err)
+    }
   })
 
   //Delete restaurant
-  app.delete("/api/v1/restaurants/:id", (req, res) => {
-    res.status(204).send({
-      status: 'success',
-      data: {
-        id: req.params.id
-      }
-    })
+  app.delete("/api/v1/restaurants/:id", async (req, res) => {
+    try {
+      const result = await db.query('delete from restaurants where id = $1 returning *', [req.params.id]);
+      res.status(204).json({
+        status: result.rows.length > 0 ? 'success' : 'No restaurant found with this id'
+      })
+    } catch (error) {
+      console.log(err)
+    }
   })
 }
