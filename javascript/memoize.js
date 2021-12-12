f962f6dd8e29933aba3a074071e24d01226d4b4e
xerxes3117 <vsnsit@gmail.com>
2021-12-13 02:09:56 +0530

    memoized function

---
+++ javascript/memoize.js
@@ -0,0 +1,51 @@
+function memoGenerator(fn) {
+  let cacheMap = new Map()
+
+  // implement your own memo
+  return function (...args) {
+    let argVal = createCachedKeys(args)
+
+    if (cacheMap.has(argVal)) {
+      console.log("from cache")
+      return cacheMap.get(argVal)
+    } else {
+      console.log("not from cache")
+      let result = fn(...args)
+      cacheMap.set(argVal, result)
+      return result
+
+    }
+  }
+}
+
+function createCachedKeys(array) {
+  return array.join('')
+}
+
+const memoArea = memoGenerator(area);
+const memoSub = memoGenerator(sub);
+const memoSum = memoGenerator(sum)
+const memoArraySum = memoGenerator(arraySum)
+
+//Testcases
+console.log(memoArea(4)) // 16 // not from cache
+console.log(memoArea(4)) // 16 // from cache
+console.log(memoArea(3)) // 9 // not from cache
+console.log(memoArea(4)) // 16 // From cache
+console.log(memoArea(3)) // 9 // from cache
+
+console.log(memoSub(4, 3)) // 1 // not from cache
+console.log(memoSub(3, 4)) // -1 // not from cache
+console.log(memoSub(4, 3)) // 1 // from cache
+console.log(memoSub(3, 4)) // -1 // from cache
+
+console.log(memoSum(4, 3)) // 7 // not from cache
+console.log(memoSum(3, 4)) // 7 // not from cache
+console.log(memoSum(4, 3)) // 7 // from cache
+console.log(memoSum(3, 4)) // 7 // from cache
+
+console.log(memoArraySum([1,2,3])) // 6 Not form cache
+console.log(memoArraySum([1,2,3])) // 6 form cache
+console.log(memoArraySum([2,3])) // 5 Not form cache
+console.log(memoArraySum([2,3])) // 5 form cache
+console.log(memoArraySum([1,2,3])) // 6 form cache
