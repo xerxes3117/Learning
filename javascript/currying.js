273adb494c08d0c83796526492e7a0874109431d
xerxes3117 <vsnsit@gmail.com>
2021-12-13 01:57:25 +0530

    variadic currying

--- javascript/currying.js
+++ javascript/currying.js
@@ -26,6 +26,7 @@ return a + b
 let curriedMultiply = curryFunc(multiply);
 let curriedAdd = curryFunc(add);
 
+//TestCases
 console.log(+curriedMultiply(1)(2)(3))
 console.log(+curriedMultiply(1)(2)(3)(4))
 console.log(+curriedAdd(1)(2)(3))
@@ -48,12 +49,48 @@ function multiply(a, b){
 
 let curried = curryFunc(multiply);
 
+//TestCases
 console.log(curried(1)(2)(3)())
 console.log(curried(1)(2)(3)(4)())
 
 
 // ------------------------------------------- With different composed argument cases (Variadic Currying) ---------------------------------------------------- //
 
-//console.log(curried(1))
-//console.log(curried(1, 2)(3)(4))
-//console.log(curried(1, 2, 3, 4))
+function curryFunc(func){
+  const innerFn = function(...outerArgs){
+    let outerSum = outerArgs.reduce(func)
+      return function(...innerArgs) {
+        if(innerArgs.length == 0){
+            return outerSum
+          } else {
+            let innerSum = innerArgs.reduce(func)
+            return innerFn(func(outerSum, innerSum))
+          }
+      }
+  }
+  return innerFn
+}
+
+function multiply(a, b){
+return a * b
+}
+
+function add(a, b){
+return a + b
+}
+
+let curriedMul = curryFunc(multiply);
+let curriedAdd = curryFunc(add);
+
+//TestCases
+console.log(curriedMul(1)(2)(3)()) //6
+console.log(curriedMul(1)(2)(3)(4)()) //24
+console.log(curriedMul(1,2)(3,4)(5)()) //120
+console.log(curriedMul(1,2,3)(4,5)(6)()) //720
+console.log(curriedAdd(1)(2)(3)()) //6
+console.log(curriedAdd(1)(2)(3)(4)()) //10
+console.log(curriedAdd(1,2)(3)(4)()) //10 
+console.log(curriedAdd(1,2)(3,4)(5)()) //15
+console.log(curriedAdd(1,2,3)(4,5)(6)()) //21 
+console.log(curriedAdd(1)(3)(4)(2)(45)());//55
+console.log(curriedAdd(1,3,4)(2)(45)());//55 
