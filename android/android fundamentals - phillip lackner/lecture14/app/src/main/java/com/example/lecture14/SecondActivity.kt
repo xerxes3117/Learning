4110f0945f6bda393e25d54d8850156d28505231
xerxes3117 <vsnsit@gmail.com>
2024-06-25 01:05:26 +0530

    android tutorials lec14 2

--- android/android fundamentals - phillip lackner/lecture14/app/src/main/java/com/example/lecture14/SecondActivity.kt
+++ android/android fundamentals - phillip lackner/lecture14/app/src/main/java/com/example/lecture14/SecondActivity.kt
@@ -1,4 +1,19 @@
 package com.example.lecture14
 
-class SecondActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lecture14.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBck.setOnClickListener {
+            finish()
+        }
+    }
 }
