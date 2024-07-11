74e3acf6be6664f5fec7a7e305f2e3bab643f004
xerxes3117 <vsnsit@gmail.com>
2024-07-11 12:16:39 +0530

    android tutorials lec19

---
+++ android/android fundamentals - phillip lackner/lecture17/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            btnChoosePhoto.setOnClickListener {
+                Intent(Intent.ACTION_GET_CONTENT).also {
+                    it.type = "image/*"
+                    startActivityForResult(it, 0)
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK && requestCode == 0) {
+            data?.data?.also {
+                binding.idPhoto.setImageURI(it)
+            }
+        }
+    }
+}
